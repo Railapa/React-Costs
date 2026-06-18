3628c4e4ba9985baa1ad278a70ab1da37bf76320
Railapa <rai100304@gmail.com>
2026-06-18 17:54:26 -0300

    React Costs

---
+++ react-costs/src/components/Layout/LinkButton.jsx
@@ -0,0 +1,12 @@
+import styles from './LinkButton.module.css'
+import {Link} from 'react-router-dom'
+
+export const LinkButton = ({to, text}) => {
+    return(
+        <div>
+            <Link to={to} className={styles.btn}>
+                {text}
+            </Link>
+        </div>
+    )
+}
--- react-costs/src/components/Pages/Home.jsx
+++ react-costs/src/components/Pages/Home.jsx
@@ -1,7 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../imgs/savings.svg'
+import { LinkButton } from '../Layout/LinkButton'
+
 export const Home = () => {
     return(
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className={styles.homeContainer}>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar seus projetos agora mesmo</p>
+            <LinkButton to="/newproject" text='Criar Projeto'>Criar Projetos</LinkButton>
+            <img src={savings} alt="costs" />
+        </section>
     )
 }
