f8f53659b2cb6953129daf187caf4bca37be9291
Railapa <rai100304@gmail.com>
2026-06-20 08:38:58 -0300

    React costs

--- react-costs/src/components/Pages/Projects.jsx
+++ react-costs/src/components/Pages/Projects.jsx
@@ -1,7 +1,18 @@
+import { Message } from "../Layout/Message"
+import {useLocation} from 'react-router-dom'
+
 export const Projects = () => {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
     return(
         <div>
-            
+            <h1>Meus Projetos</h1>
+            {message && <Message msg={message} type='success'/>}
         </div>
     )
 }
