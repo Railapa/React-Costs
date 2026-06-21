9bf76829cfab69ddc77ab7581a03e7c49c5e64aa
Railapa <rai100304@gmail.com>
2026-06-21 10:08:41 -0300

    React Costs

--- react-costs/src/components/Pages/Projects.jsx
+++ react-costs/src/components/Pages/Projects.jsx
@@ -1,18 +1,27 @@
 import { Message } from "../Layout/Message"
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import styles from './Projects.module.css'
+import {Container} from '../Layout/Container'
+import {LinkButton} from '../Layout/LinkButton'
 
 export const Projects = () => {
 
     const location = useLocation()
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
     }
 
-    return(
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message msg={message} type='success'/>}
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto' />
+            </div>
+            {message && <Message msg={message} type='success' />}
+            <Container custonClass='start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
